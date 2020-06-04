1cb7414ec73db0475df8bb416e0bb6030cbca3eb
Giosue1994 <giosuelentini1994@outlook.com>
2020-06-04 17:54:08 +0200

    generare 16 numeri casuali tra 1 e 100

--- js/script.js
+++ js/script.js
@@ -13,66 +13,31 @@
 // Con difficoltà 0=> tra 1 e 100, con difficoltà 1 => tra 1 e 80, con
 // difficoltà 2=> tra 1 e 50
 
-// FUNZIONE NUMERI RANDOM
-function nRandom(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) ) + min;
-}
-
-// FUNZIONE 16 NUMERI RANDOM
-function nRandom16(bomb) {
-  var bomb = [];
-  for (var i = 0; i < 16; i++) {
-    bomb.push(nRandom(1, 100))
-  }
-
-  return bomb;
-}
-
 // VARIABILI
-var listaBombe;
-var numeroUtente;
-var listaNumeriUtente = [];
-var message;
+var rangeNumeri = 100;
+var bombe = 16;
 
-var bomba = false;
+// creo un array vuoto dove vengono inseriti i 16 numeri casuali
+var listaBombe = [];
 
 // genero 16 numeri casuali tra 1 e 100
-listaBombe = nRandom16(listaBombe);
-console.log(listaBombe);
+while (listaBombe.length < bombe) {
+  var nRandom = Math.floor(Math.random() * rangeNumeri) + 1;
 
+// Controllare che il numero creato non sia dentro a listaBombe
+  // specifico che inizialmente in numero non viene trovato
+  var trovato = false;
 
-// chiedo all'utente di inserire un numero compreso tra 1 e 100
-while (bomba === false && listaNumeriUtente.length < 84) {
-  numeroUtente = parseInt(prompt('Inserisci un numero'));
-
-  // verifico se l'elemento inserito dall'utente è già presnte nella lista
-  for (var j = 0; j < listaNumeriUtente.length; j++) {
-    if (numeroUtente === listaNumeriUtente[j]) {
-      alert('Numero già inserito');
-      bomba = true;
-      message = 'HAI PERSO';
-      console.log(message);
-    }
-  }
-  
-  // inserisco i numeri dichiarati dall'utente in una lista
-  listaNumeriUtente.push(numeroUtente);
-  console.log('lista dei numeri inseriti dall\'utente ', listaNumeriUtente);
-
-
-  // verifico che l'elemento non sia nella lista
+  // creo un ciclo per controllare se il numero già inserito è stato trovato
   for (var i = 0; i < listaBombe.length; i++) {
-    if (numeroUtente === listaBombe[i]) {
-      alert('BOOM!')
-      bomba = true;
-      message = 'HAI PERSO!';
-      console.log(message);
+    if (nRandom === listaBombe[i]) {
+      trovato = true;
     }
   }
 
-  // se non viene trovata nessuna bomba l'utente ha vinto
-  if (listaNumeriUtente.length == 84) {
-    message = 'HAI VINTO!';
-    console.log(message);
+  // se il numero non è uguale viene inserito nella lista
+  if (trovato === false) {
+    listaBombe.push(nRandom);
   }
 }
+console.log(listaBombe);
